57abf6a557cda8a5150ccd941936063dc7ad79a9
RetepSativa <avaede@live.dk>
2022-05-23 12:46:13 +0200

    adka

--- GucciFestival/Shared/Models/Coordinator.cs
+++ GucciFestival/Shared/Models/Coordinator.cs
@@ -8,13 +8,14 @@ namespace GucciFestival.Shared.Models
         {
             //Her er de properties vi gerne vil læse fra vores shelterdatabase
             [Required]
-            public string Cvr_navn { get; set; }
+            public string Cvr_navn { get; set; 
+        public string Cvr_navnsaa { get; set; }
 
-            
 
-            //en tom constructor. Dem er vi glade for :)
-            
-        }
+
+        //en tom constructor. Dem er vi glade for :)
+
+    }
     }
 
 
