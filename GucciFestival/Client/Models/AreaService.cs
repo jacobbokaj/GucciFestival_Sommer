04949fd1745b72e6a2ae651e98c6eadc32e2fb79
Sebastian Søborg Aagesen <eaaseaa@students.eaaa.dk>
2022-05-24 10:55:54 +0200

    djgfg

--- GucciFestival/Client/Models/AreaService.cs
+++ GucciFestival/Client/Models/AreaService.cs
@@ -14,12 +14,19 @@ namespace GucciFestival.Client.Models
         {
             this.httpClient = httpClient;
         }
+        //Her henter vi data fra vores api og får det som en JSON fil
+        //Vi bruger Async til at sørge for alt dataen er hentet før det loader
         public async Task<Area[]?> GetAllAreas()
         {
             var result = await httpClient.GetFromJsonAsync<Area[]>("api/areaapi");
 
             return result;
         }
-      
+        public async Task<int> AddCoordinator(Area area)
+        {
+            var result = await httpClient.PostAsJsonAsync<Area>("api/areaapi", area);
+            return (int)result.StatusCode;
+
+        }
     }
 }
