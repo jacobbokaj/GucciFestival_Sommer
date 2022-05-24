2734742e42f26ddbbd9b1f4dbbe838e656dcfd69
jacobbokaj <jacobvestergaard@hotmail.dk>
2022-05-24 09:59:14 +0200

    Hej :D

--- GucciFestival/Client/Models/ShiftService.cs
+++ GucciFestival/Client/Models/ShiftService.cs
@@ -22,6 +22,9 @@ namespace GucciFestival.Client.Models
         /// <summary>
         /// Har kommunikation med "shiftapi", som er "ShiftController" i "GucciFestival.Server".
         /// Siden det er httpclient, bruger den [HttpGet] for at få dataen.
+        /// Det kommer som Json fil, derfor skal "GetFromJsonAsync" bruges for at lave det til data der kan ligges i en klasse.
+        /// Async Venter til at alt dataen er med, før den melder det er udført. Async køre i et andet sted for at hjemmesiden ikke vil lag til det er hentet.
+        /// Hvilket gør at det kan være svært nogle gange at vide, hvornår den er færdig.
         /// </summary>
         /// <returns></returns>
         public async Task<Shift[]?> GetAllShifts()
