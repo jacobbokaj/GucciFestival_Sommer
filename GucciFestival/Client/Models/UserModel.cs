8d647f8563fbf8d2db234dd082785a3909704e0a
Sebastian Søborg Aagesen <eaaseaa@students.eaaa.dk>
2022-05-25 09:46:15 +0200

    Usermodel

---
+++ GucciFestival/Client/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using GucciFestival.Shared.Models;
+
+
+namespace GucciFestival.Client.Models
+{
+    //Hej
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; } = DateTime.Now;
+
+        public string Email { get; set; }
+
+        public int Tlf { get; set; }
+
+        public string Password { get; set; }
+
+        public List <Competences> Competencelist { get; set; }
+    }
+}
