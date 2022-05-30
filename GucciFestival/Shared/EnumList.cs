0d18ce05b5ce54e3e06c33f09a660b32ca172a2b
jacobbokaj <jacobvestergaard@hotmail.dk>
2022-05-30 09:14:07 +0200

    Navmen and static

---
+++ GucciFestival/Shared/EnumList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GucciFestival.Shared
+{
+
+    namespace NavigateMenus
+    {
+        public enum NavigateMenus
+        {
+            MainSide,
+            Coordinator,
+            Volenteer
+        }
+
+    }
+    namespace VolenteerPages
+    {
+        public enum VolenteerPages
+        {
+            ShiftsAvailable,
+            ShiftsOwn,
+            ShiftRead
+        }
+    }
+}
